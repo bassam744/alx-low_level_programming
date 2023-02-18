cfc7622fdca85b6535a87b7b4ca5f7c0b10b9ed0
bassam744 <ba818942@gmail.com>
2023-02-18 13:58:24 -0800

    dd

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
  * main -print a line of code using puts
- * return always 0
+ * return: always 0
  */
 int  main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-		return (0);
+return (0);
 }
